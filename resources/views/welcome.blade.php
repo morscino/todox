a28d40a8a5edd8e4be7958f4cb489ebede915246
morscino <morscino@gmail.com>
2019-10-27 10:57:15 +0000

    Fixed modal edit issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{csrf_token()}}">
 
-        <title>dox</title>
+        <title>Todox</title>
         <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
        
     </head>
